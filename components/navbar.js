5c5478c37f9c2be815b99b4f451a2c3fed386f3a
tanner9427 <tanner9427@gmail.com>
2024-06-21 19:49:35 -0700

    navbar format and icons #slay

--- components/navbar.js
+++ components/navbar.js
@@ -4,22 +4,24 @@ import styles from './navbar.module.css';
 export default function Navbar() {
     return (
         <div className={styles.navbar}>
+            <a className={styles.name}>Tanner Tran</a>
             <ul className={styles.items}>
                 <li className={styles.item}>
-                    <Link href="profile">
-                        <img src="/images/profileimg.svg" alt="Profile" className={styles.icon} />
-                    </Link>
-                </li>
-                <li className={styles.item}>
-                    <Link href="">
+                    <Link href="work">
                     </Link>
                 </li>
                 <li className={styles.item}>
                     <Link href="resume">
+                        <img src="/images/resume.svg" alt="About Me" className={styles.icon} />
                     </Link>
                 </li>
                 <li className={styles.item}>
                     <Link href="contact">
+                    </Link>
+                </li>
+                <li className={styles.item}>
+                    <Link href="about me">
+                        <img src="/images/profileimg.svg" alt="About Me" className={styles.icon} />
                     </Link>
                 </li>
             </ul>
