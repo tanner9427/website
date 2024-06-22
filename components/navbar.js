97cb12421ace3662aba656b70d145e7a18d3d2c2
tanner9427 <tanner9427@gmail.com>
2024-06-22 16:20:33 -0700

    more nav

--- components/navbar.js
+++ components/navbar.js
@@ -3,28 +3,34 @@ import styles from './navbar.module.css';
 
 export default function Navbar() {
     return (
-        <div className={styles.navbar}>
-            <a className={styles.name}>Tanner Tran</a>
-            <ul className={styles.items}>
-                <li className={styles.item}>
-                    <Link href="work">
-                    </Link>
-                </li>
-                <li className={styles.item}>
-                    <Link href="resume">
-                        <img src="/images/resume.svg" alt="About Me" className={styles.icon} />
-                    </Link>
-                </li>
-                <li className={styles.item}>
-                    <Link href="contact">
-                    </Link>
-                </li>
-                <li className={styles.item}>
-                    <Link href="about me">
-                        <img src="/images/profileimg.svg" alt="About Me" className={styles.icon} />
-                    </Link>
-                </li>
-            </ul>
+        <div className={styles.navcontainer}>
+            <div className={styles.navbar}>
+                <a className={styles.name}>Tanner Tran</a>
+                <ul className={styles.items}>
+                    <li className={styles.item}>
+                        <Link href="work">
+                            work
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link href="resume">
+                            {/* <img src="/images/resumeline.svg" alt="About Me" className={styles.icon} /> */}
+                            resume
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link href="about me">
+                            {/* <img src="/images/profileimg.svg" alt="About Me" className={styles.icon} /> */}
+                            about me
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link href="contact">
+                            contact me
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
