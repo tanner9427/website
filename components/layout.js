e69bd795e77edcc9e647684fbfcce64e6861a8d4
tanner9427 <tanner9427@gmail.com>
2025-10-12 17:22:25 -0800

    favicon fixes

--- components/layout.js
+++ components/layout.js
@@ -145,7 +145,12 @@ export default function Layout({ children, home, showHeader = false, showNavbar 
     return (
         <div className={styles.outerContainer}>
             <Head>
-                <link rel="icon" type="image/png" href="/favicon.png" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <meta name="theme-color" content="#051D3C" />
                 <meta name="Tanner Tran Website" content="Tanner Tran's Personal Website" />
                 <meta
                     property="og:image"
